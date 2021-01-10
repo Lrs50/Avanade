01d3cf06440cd10a185ba968001782bb7f6c0ba1
Lucas Reis <lrs5@cin.ufpe.br>
2021-01-10 14:29:03 -0300

    Testing .NET SDKT

---
+++ C#/Test1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=5;
+            for(int i=0;i<n;i++){
+                Console.WriteLine($".NET {i}");
+            }
+        }
+    }
+}
